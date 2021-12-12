301e3a9e21fd7f20113412e982872c094fc43b0b
Nathaniel Scholnick <nate.scholnick@gmail.com>
2021-12-12 03:48:49 -0500

    added proxy for cors workaround in dev

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// 76561198350848550
+// 76561198350848550 64bit tho
+// 76561198067021926
+// 76561197960265728
+
+// 390582822 cabbeg mage
+// 106756198 citius
 
 const App = () => {
   const [playerId, setPlayerId] = useState('');
   const [player, setPlayer] = useState('');
   const getPlayer = (playerId) => {
     if(!playerId) return;
-    fetch(`https://api.opendota.com/api/players/${playerId}`).then(
+    // fetch(`/players/${playerId}`).then(
+    fetch(`/players/${playerId}`).then(
     (res) => {
       if (!res.ok) {
         throw new Error(res.statusText);
       }
       return res.json();
     }).then((data) => {
+      console.log(data);
       setPlayer(data);
     })
     .catch(err => {
       throw new Error(err)
     });
   };
-
-  console.log(playerId);
 
   return (
     <div className="App">
@@ -43,12 +48,20 @@ const App = () => {
           />
         </form>
         <button
-          onClick={getPlayer(playerId)}
+          onClick={() => getPlayer(playerId)}
         >
           Get Player
         </button>
       </header>
-      <p>{Object.keys(player).join(' ')}</p>
+      {player && (
+      <div className="playerInfo">
+        <img src={player.profile.avatarfull} alt="player avatar" />
+        <a target="_blank" rel="noreferrer" href={player.profile.profileurl} className="playerName">
+          <h2>{player.profile.personaname}</h2>
+        </a>
+        <p className="mmr">MMR: {player.mmr_estimate.estimate}</p>
+      </div>
+      )}
     </div>
   );
 }
